bd608c62fbd1c44001ed01198260ab30f724c58b
usingforschool <formyhackforums@gmail.com>
2019-11-29 11:09:03 +0100

    lijp

--- cart.php
+++ cart.php
@@ -1,21 +1,27 @@
-<?php require_once './resources/layouts/header.php';
+<?php
+require_once './functions/sessions.php';
 
+
+require_once './resources/layouts/header.php';
+
+$card = [];
 ?>
 
 <main>
     <div class="cart_section">
         <link href="resources/css/cart.css" rel="stylesheet">
         <div class="row">
-            <div class="col">Productnaam</div>
-            <div class="col">Prijs</div>
-            <div class="col">Aantal</div>
-            <div class="col">Totaal</div>
-        </div>
-        <div class="row">
-            <div class="col-3">USB stick</div>
-            <div class="col-3">29.99</div>
-            <div class="col-3">4</div>
-            <div class="col-3">119.96</div>
+            <div class="col-3">Productnaam</div>
+            <div class="col-3">Prijs</div>
+            <div class="col-3">Aantal</div>
+            <div class="col-3">Totaal</div>
+
+            <?php foreach ($card as $item):?>
+                <div class="col-3"><?=$item['itemname']?></div>
+                <div class="col-3">29.99</div>
+                <div class="col-3">4</div>
+                <div class="col-3">119.96</div>
+            <?php endforeach;?>
         </div>
 
 
