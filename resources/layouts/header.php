9c38409f22dcb838b56b2ebd7fd9b97f2ef07307
Mark Wierda <m.wierda@connectingthedots.nl>
2019-12-03 13:27:01 +0100

    Fixed homepage link

--- resources/layouts/header.php
+++ resources/layouts/header.php
@@ -44,7 +44,7 @@ $categories = getCategories();
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
     <div class="container">
         <a class="navbar-brand" href="#"></a>
-        <a href="http://localhost"><img src="resources\images\wide-world-importers-logo-small.png" style="max-height: 60px; padding-right: 100px" alt=""></a>
+        <a href="index.php"><img src="resources\images\wide-world-importers-logo-small.png" style="max-height: 60px; padding-right: 100px" alt=""></a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
